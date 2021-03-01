cc57094fa9e0775bd6b35ad794296bc31d6c62dc
Marius Dervinis <mariux@hotmail.lt>
2021-03-01 13:22:06 +0200

    updated to specify background element better

--- script.js
+++ script.js
@@ -11,131 +11,133 @@ var modeButtons = document.querySelectorAll(".mode");
 
 init();
 
-function init(){
-	setupModeButtons();
-	setupSquares();
-	reset();
+function init() {
+    setupModeButtons();
+    setupSquares();
+    reset();
 }
 
-function setupModeButtons(){
-	for(var i = 0; i < modeButtons.length; i++){
-		modeButtons[i].addEventListener("click", function(){
-			modeButtons[0].classList.remove("selected");
-			modeButtons[1].classList.remove("selected");
-			this.classList.add("selected");
-			this.textContent === "Easy" ? numSquares = 3: numSquares = 6;
-			reset();
-		});
-	}
+function setupModeButtons() {
+    for (var i = 0; i < modeButtons.length; i++) {
+        modeButtons[i].addEventListener("click", function() {
+            modeButtons[0].classList.remove("selected");
+            modeButtons[1].classList.remove("selected");
+            this.classList.add("selected");
+            this.textContent === "Easy" ? numSquares = 3 : numSquares = 6;
+            reset();
+        });
+    }
 }
 
-function setupSquares(){
-	for(var i = 0; i < squares.length; i++){
-	//add click listeners to squares
-		squares[i].addEventListener("click", function(){
-			//grab color of clicked square
-			var clickedColor = this.style.background;
-			//compare color to pickedColor
-			if(clickedColor === pickedColor){
-				messageDisplay.textContent = "Correct!";
-				resetButton.textContent = "Play Again?"
-				changeColors(clickedColor);
-				h1.style.background = clickedColor;
-			} else {
-				this.style.background = "#232323";
-				messageDisplay.textContent = "Try Again"
-			}
-		});
-	}
+function setupSquares() {
+    for (var i = 0; i < squares.length; i++) {
+        //add click listeners to squares
+        squares[i].addEventListener("click", function() {
+            //grab color of clicked square
+            var clickedColor = this.style.backgroundColor;
+            //compare color to pickedColor
+            if (clickedColor === pickedColor) {
+                console.log(clickedColor + " is equal to " + pickedColor);
+                messageDisplay.textContent = "Correct!";
+                resetButton.textContent = "Play Again?"
+                changeColors(clickedColor);
+                h1.style.background = clickedColor;
+            } else {
+                console.log(clickedColor + " is not equal to " + pickedColor);
+                this.style.backgroundColor = "#232323";
+                messageDisplay.textContent = "Try Again"
+            }
+        });
+    }
 }
 
 
 
-function reset(){
-	colors = generateRandomColors(numSquares);
-	//pick a new random color from array
-	pickedColor = pickColor();
-  
-  //Filtering RGB color string just to a numbers and excluding to each color
-	rgbfilter = pickedColor.replace(/[^\d,]/g, '').split(',');
-	var rgbcR = parseInt(rgbfilter[0], 10);
-	var rgbcG = parseInt(rgbfilter[1], 10);
-	var rgbcB = parseInt(rgbfilter[2], 10);
-  
-  //Converting RGB colors to Hex for color names in ntc.js code
-	function componentToHex(c) {
-		var hex = c.toString(16);
-		return hex.length == 1 ? "0" + hex : hex;
-	  }
-	  
-	  function rgbToHex(r, g, b) {
-		return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
-	  }
-	
-	var Mycolor = (rgbToHex(rgbcR, rgbcG, rgbcB));
-	
-	var n_match  = ntc.name(Mycolor);
-    n_rgb        = n_match[0]; // This is the RGB value of the closest matching color
-    n_name       = n_match[1]; // This is the text string for the name of the match
+function reset() {
+    colors = generateRandomColors(numSquares);
+    //pick a new random color from array
+    pickedColor = pickColor();
+
+    //Filtering RGB color string just to a numbers and excluding to each color
+    rgbfilter = pickedColor.replace(/[^\d,]/g, '').split(',');
+    var rgbcR = parseInt(rgbfilter[0], 10);
+    var rgbcG = parseInt(rgbfilter[1], 10);
+    var rgbcB = parseInt(rgbfilter[2], 10);
+
+    //Converting RGB colors to Hex for color names in ntc.js code
+    function componentToHex(c) {
+        var hex = c.toString(16);
+        return hex.length == 1 ? "0" + hex : hex;
+    }
+
+    function rgbToHex(r, g, b) {
+        return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
+    }
+
+    var Mycolor = (rgbToHex(rgbcR, rgbcG, rgbcB));
+
+    var n_match = ntc.name(Mycolor);
+    n_rgb = n_match[0]; // This is the RGB value of the closest matching color
+    n_name = n_match[1]; // This is the text string for the name of the match
     n_exactmatch = n_match[2]; // True if exact color match, False if close-match
 
 
 
-	var Colorname = n_match[1]; //
+    var Colorname = n_match[1]; //
 
-	//change colorDisplay to match picked Color
-	colorDisplay.textContent = Colorname;
-	resetButton.textContent = "New Colors"
-	messageDisplay.textContent = "";
-	//change colors of squares
-	for(var i = 0; i < squares.length; i++){
-		if(colors[i]){
-			squares[i].style.display = "block"
-			squares[i].style.background = colors[i];
-		} else {
-			squares[i].style.display = "none";
-		}
-	}
-	h1.style.background = "steelblue";
+    //change colorDisplay to match picked Color
+    colorDisplay.textContent = Colorname;
+    resetButton.textContent = "New Colors"
+    messageDisplay.textContent = "";
+    //change colors of squares
+    for (var i = 0; i < squares.length; i++) {
+        if (colors[i]) {
+            squares[i].style.display = "block"
+            squares[i].style.background = colors[i];
+        } else {
+            squares[i].style.display = "none";
+        }
+    }
+    h1.style.background = "steelblue";
 }
 
-resetButton.addEventListener("click", function(){
-	reset();
+resetButton.addEventListener("click", function() {
+    reset();
 })
 
-function changeColors(color){
-	//loop through all squares
-	for(var i = 0; i < squares.length; i++){
-		//change each color to match given color
-		squares[i].style.background = color;
-	}
+function changeColors(color) {
+    //loop through all squares
+    for (var i = 0; i < squares.length; i++) {
+        //change each color to match given color
+        squares[i].style.background = color;
+    }
 }
 
-function pickColor(){
-	var random = Math.floor(Math.random() * colors.length);
-	return colors[random];
+function pickColor() {
+    var random = Math.floor(Math.random() * colors.length);
+    return colors[random];
 }
 
-function generateRandomColors(num){
-	//make an array
-	var arr = []
-	//repeat num times
-	for(var i = 0; i < num; i++){
-		//get random color and push into arr
-		arr.push(randomColor())
-	}
-	//return that array
-	return arr;
+function generateRandomColors(num) {
+    //make an array
+    var arr = []
+        //repeat num times
+    for (var i = 0; i < num; i++) {
+        //get random color and push into arr
+        arr.push(randomColor())
+    }
+    //return that array
+    return arr;
 }
 
-function randomColor(){
-	//pick a "red" from 0 - 255
-	var r = Math.floor(Math.random() * 256);
-	//pick a "green" from  0 -255
-	var g = Math.floor(Math.random() * 256);
-	//pick a "blue" from  0 -255
-	var b = Math.floor(Math.random() * 256);
-	return "rgb(" + r + ", " + g + ", " + b + ")";
+function randomColor() {
+    //pick a "red" from 0 - 255
+    var r = Math.floor(Math.random() * 256);
+    //pick a "green" from  0 -255
+    var g = Math.floor(Math.random() * 256);
+    //pick a "blue" from  0 -255
+    var b = Math.floor(Math.random() * 256);
+    return "rgb(" + r + ", " + g + ", " + b + ")";
 }
 
 
@@ -324,4 +326,3 @@ function randomColor(){
 //   var b = Math.floor(Math.random() * 256);
 //   return "rgb(" + r + ", " + g + ", " + b + ")";
 // }
-
